a9ae5e6eefb982b46a43e11c747533d2df1d682b
chistym17 <mchisty31@gmail.com>
2024-12-25 21:21:41 +0600

    deployed to local net using anvil

---
+++ script/deployment.s.sol
@@ -0,0 +1,15 @@
+//spdx-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../contract/campaign.sol";
+
+contract DeployCampaign is Script{
+    function run() external{
+        vm.startBroadcast();
+        new Campaign();
+        vm.stopBroadcast();
+    }
+}
+
+
